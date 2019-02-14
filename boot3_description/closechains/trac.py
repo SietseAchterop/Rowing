ca9b13a2c51cf2b8656afcab4d611cf4ef4a4d02
Sietse Achterop <s.achterop@rug.nl>
2019-02-14 19:48:28 +0100

    more or less stable version, changed dynamics values

---
+++ boot3_description/closechains/trac.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from trac_ik_python.trac_ik import IK
+
+ik_solver = IK("skiff",  "dummy_link_1")
+
+print(ik_solver.base_link)
+print(ik_solver.tip_link)
+print(ik_solver.joint_names)
+print(ik_solver.link_names)
